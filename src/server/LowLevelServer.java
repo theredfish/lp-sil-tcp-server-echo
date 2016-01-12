46c8d8e34650248c96af3daafd279e0c0aed0067
theredfish <did.julian@gmail.com>
2016-01-12 18:51:51 +0100

    Low level server skeleton

--- src/server/LowLevelServer.java
+++ src/server/LowLevelServer.java
@@ -1,13 +1,37 @@
 package server;
-import services.EchoService;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import client.Client;
 
 public class LowLevelServer extends AbstractServer {
 
-	/**
-	 * Run low level server as a (echo) service
-	 */
-	public void launch() {
-		// TODO implementation
-	}
+    /**
+     * Low level server constructor
+     */
+    public LowLevelServer() {
+        super();
+    }
 
+	/**
+	 * Launch low level server as a (echo) service
+	 */
+    public void launch() {
+		try {
+			socket = new ServerSocket(port);
+			System.out.println("TCP low level server is running on " + this.port + "...");
+
+			// Server in listen mode
+			while (true) {
+				Socket tcpClient = socket.accept();
+				Client client = new Client(tcpClient, responseDelay);
+
+				// Here we run new "client" thread
+				new Thread(client).start();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
